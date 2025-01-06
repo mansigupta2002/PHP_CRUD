fc5b7d5ce3406bb842c36a6cf35fe027a7d3ad8b
Mansi Gupta <81900454+mansigupta2002@users.noreply.github.com>
2025-01-06 11:37:17 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "myshop";
+
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    $sql = "DELETE FROM clients WHERE id=$id";
+
+    if ($connection->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $connection->error;
+    }
+
+    $connection->close();
+}
+
+header("location:/CRUD/index.php");
+exit;
+?>
